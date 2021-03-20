ba5fcd8991c6279abe998a4e9f5e9749a9b6d5f0
Isaayy <kuba.serwin99@gmail.com>
2021-03-20 22:52:23 +0100

    Fetch data from server 1/2

--- frontend/src/Components/Input/Input.js
+++ frontend/src/Components/Input/Input.js
@@ -6,7 +6,7 @@ const Input = (props) => {
     return (
         <div className="input">
             <label className="input__label" for={props.name}>{props.children}</label>
-            <input className="input__input" type="text" id={props.name}/>
+            <input className="input__input" type="text" id={props.name} onBlur={props.blur}/>
         </div>
     )
 }
--- frontend/src/Components/Main/Main.js
+++ frontend/src/Components/Main/Main.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Main.scss'
 import Input from '../Input/Input';
 import Output from '../Output/Output';
 
 const Main = () => {
+    const [values,updateValues] = useState({});
+
+    const addValue = (e) => {
+        const valCpy = values;
+        valCpy[`${e.target.id}`] = e.target.value;
+        updateValues(valCpy)
+    }
+
+    const calculate = () => {
+        let url = '127.0.0.1:8080/api/system/get?';
+        for (const value in values) {
+            url += `${value}=${values[value]}&`
+        }
+
+        fetch(url.slice(0,-1))
+        .then(response => {
+          if (!response.ok) throw new Error('Couldn\'t fetch data');
+          console.log(response);
+          console.log(response.text());
+        //   return response.json();
+        })
+        .then(data => {
+            // console.log(data);
+        //   const countriesCopy = [...state.countries]
+        //   countriesCopy.push(data[0]);
+        //   updateState({countries:countriesCopy })
+        })
+        .catch(err => alert(err))
+    }
+
     return (
         <main className="main">
             <div className="main__content">
                 <h2 className="main__heading">Dane wejściowe</h2>
-                <Input name="lambda">Liczba zgłoszeń napływających w jednostce czasu <span>(λ)</span></Input>
-                <Input name="mu">Liczba zgłoszeń obsługiwanych w jednostce czasu <span>(μ)</span></Input>
-                <Input name="m">Liczba dostępnych kanałów obsługi <span>(m)</span></Input>
-                <Input name="c1">Nie wiadomo <span>(C<sub>1</sub>)</span></Input>
-                <Input name="c2">Nie wiadomo <span>(C<sub>2</sub>)</span></Input>
-                <button className="main__button">Oblicz</button>
+                <Input name="lambda" blur={addValue}>Liczba zgłoszeń napływających w jednostce czasu <span>(λ)</span></Input>
+                <Input name="mu" blur={addValue}>Liczba zgłoszeń obsługiwanych w jednostce czasu <span>(μ)</span></Input>
+                <Input name="m" blur={addValue}>Liczba dostępnych kanałów obsługi <span>(m)</span></Input>
+                <Input name="c1" blur={addValue}>Nie wiadomo <span>(C<sub>1</sub>)</span></Input>
+                <Input name="c2" blur={addValue}>Nie wiadomo <span>(C<sub>2</sub>)</span></Input>
+                <button onClick={calculate} className="main__button">Oblicz</button>
             </div>
             <div className="main__content main__content--results">
                 <h2 className="main__heading">Wyniki</h2>
