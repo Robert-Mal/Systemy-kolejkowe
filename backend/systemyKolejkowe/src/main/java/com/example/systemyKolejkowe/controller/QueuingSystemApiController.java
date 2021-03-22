bd6012973496ad467cb759ce3b57ae2c7a032b7e
Robert-Mal <33180@student.pwsztar.edu.pl>
2021-03-22 10:54:35 +0100

    CORS configuration added

--- backend/systemyKolejkowe/src/main/java/com/example/systemyKolejkowe/controller/QueuingSystemApiController.java
+++ backend/systemyKolejkowe/src/main/java/com/example/systemyKolejkowe/controller/QueuingSystemApiController.java
@@ -3,12 +3,8 @@ package com.example.systemyKolejkowe.controller;
 import com.example.systemyKolejkowe.dto.QueuingSystemDto;
 import com.example.systemyKolejkowe.service.QueuingSystemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -17,6 +13,7 @@ public class QueuingSystemApiController {
     @Autowired
     QueuingSystemService queuingSystemService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value = "/system/get", method = RequestMethod.GET)
     public ResponseEntity<QueuingSystemDto> testDocument(@RequestParam(value = "lambda") float lambda,
                                              @RequestParam(value = "mu") float mu,
@@ -26,7 +23,8 @@ public class QueuingSystemApiController {
 
         QueuingSystemDto queuingSystemDto = queuingSystemService.getQueuingSystem(lambda, mu, m, c1, c2);
 
-        return new ResponseEntity<>(queuingSystemDto, HttpStatus.OK);
-
+        //return new ResponseEntity<>(queuingSystemDto, HttpStatus.OK);
+        return ResponseEntity.ok(queuingSystemDto);
     }
+
 }
