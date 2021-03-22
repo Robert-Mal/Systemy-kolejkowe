14a533c7e6b68b8079b60e47281ae8bd8caa0fec
Isaayy <kuba.serwin99@gmail.com>
2021-03-22 15:05:12 +0100

    Fetching data from server

--- frontend/src/Components/Main/Main.js
+++ frontend/src/Components/Main/Main.js
@@ -1,66 +1,126 @@
 import React, { useState } from 'react';
 
-import './Main.scss'
+import './Main.scss';
 import Input from '../Input/Input';
 import Output from '../Output/Output';
 
 const Main = () => {
-    const [values,updateValues] = useState({});
+  const [state, updateState] = useState({ inputVal: {}, outputVal: [] });
 
-    const addValue = (e) => {
-        const valCpy = values;
-        valCpy[`${e.target.id}`] = e.target.value;
-        updateValues(valCpy)
+  const addValue = e => {
+    const valCpy = state.inputVal;
+    valCpy[`${e.target.id}`] = e.target.value;
+    updateState({
+      inputVal: valCpy,
+      outputVal: state.outputVal,
+    });
+  };
+
+  const calculate = () => {
+    let url = 'http://127.0.0.1:8080/api/system/get?';
+    for (const value in state.inputVal) {
+      url += `${value}=${state.inputVal[value]}&`;
     }
 
-    const calculate = () => {
-        let url = '127.0.0.1:8080/api/system/get?';
-        for (const value in values) {
-            url += `${value}=${values[value]}&`
-        }
+    fetch(url.slice(0, -1))
+      .then(response => {
+        if (!response.ok) throw new Error("Couldn't fetch data");
+        return response.json();
+      })
+      .then(data => {
+        updateState({
+          inputVal: state.inputVal,
+          outputVal: Object.values(data),
+        });
+      })
+      .catch(err => alert(err));
+  };
 
-        fetch(url.slice(0,-1))
-        .then(response => {
-          if (!response.ok) throw new Error('Couldn\'t fetch data');
-          console.log(response);
-          console.log(response.text());
-        //   return response.json();
-        })
-        .then(data => {
-            // console.log(data);
-        //   const countriesCopy = [...state.countries]
-        //   countriesCopy.push(data[0]);
-        //   updateState({countries:countriesCopy })
-        })
-        .catch(err => alert(err))
-    }
-
-    return (
-        <main className="main">
-            <div className="main__content">
-                <h2 className="main__heading">Dane wejściowe</h2>
-                <Input name="lambda" blur={addValue}>Liczba zgłoszeń napływających w jednostce czasu <span>(λ)</span></Input>
-                <Input name="mu" blur={addValue}>Liczba zgłoszeń obsługiwanych w jednostce czasu <span>(μ)</span></Input>
-                <Input name="m" blur={addValue}>Liczba dostępnych kanałów obsługi <span>(m)</span></Input>
-                <Input name="c1" blur={addValue}>Nie wiadomo <span>(C<sub>1</sub>)</span></Input>
-                <Input name="c2" blur={addValue}>Nie wiadomo <span>(C<sub>2</sub>)</span></Input>
-                <button onClick={calculate} className="main__button">Oblicz</button>
-            </div>
-            <div className="main__content main__content--results">
-                <h2 className="main__heading">Wyniki</h2>
-                <div className="main__results">
-                    <Output>Prawdopodobieństwo, że system jest pusty <span>(P<sub>0</sub>)</span></Output>
-                    <Output>Średnia długość kolejki <span>(<p>v</p>)</span></Output>
-                    <Output>Średnia liczba zgłoszeń w systemie <span>(<p>n</p>)</span></Output>
-                    <Output>Średnia liczba zgłoszeń w obsłudze <span>(<p>m</p><sub>0</sub>)</span></Output>
-                    <Output>Średni czas oczekiwania zgłoszenia w kolejce <span>(<p>t</p><sub>t</sub>)</span></Output>
-                    <Output>Średni czas przesycania zgłoszenia w systemie <span>(<p>t</p><sub>s</sub>)</span></Output>
-                    <Output>Średnia liczba nie zajętych kanałów obsługi <span>(<p>m</p><sub>nz</sub>)</span></Output>
-                    <Output>Wartość funkcji celu <span>(F(m))</span></Output>
-                </div>
-            </div>
-        </main>
-    )
-}
+  return (
+    <main className="main">
+      <div className="main__content">
+        <h2 className="main__heading">Dane wejściowe</h2>
+        <Input name="lambda" blur={addValue}>
+          Liczba zgłoszeń napływających w jednostce czasu <span>(λ)</span>
+        </Input>
+        <Input name="mu" blur={addValue}>
+          Liczba zgłoszeń obsługiwanych w jednostce czasu <span>(μ)</span>
+        </Input>
+        <Input name="m" blur={addValue}>
+          Liczba dostępnych kanałów obsługi <span>(m)</span>
+        </Input>
+        <Input name="c1" blur={addValue}>
+          Nie wiadomo{' '}
+          <span>
+            (C<sub>1</sub>)
+          </span>
+        </Input>
+        <Input name="c2" blur={addValue}>
+          Nie wiadomo{' '}
+          <span>
+            (C<sub>2</sub>)
+          </span>
+        </Input>
+        <button onClick={calculate} className="main__button">
+          Oblicz
+        </button>
+      </div>
+      <div className="main__content main__content--results">
+        <h2 className="main__heading">Wyniki</h2>
+        <div className="main__results">
+          <Output val={state.outputVal[0]}>
+            Prawdopodobieństwo, że system jest pusty{' '}
+            <span>
+              (P<sub>0</sub>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[1]}>
+            Średnia długość kolejki{' '}
+            <span>
+              (<p>v</p>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[2]}>
+            Średnia liczba zgłoszeń w systemie{' '}
+            <span>
+              (<p>n</p>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[3]}>
+            Średnia liczba zgłoszeń w obsłudze{' '}
+            <span>
+              (<p>m</p>
+              <sub>0</sub>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[4]}>
+            Średni czas oczekiwania zgłoszenia w kolejce{' '}
+            <span>
+              (<p>t</p>
+              <sub>t</sub>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[5]}>
+            Średni czas przesycania zgłoszenia w systemie{' '}
+            <span>
+              (<p>t</p>
+              <sub>s</sub>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[6]}>
+            Średnia liczba nie zajętych kanałów obsługi{' '}
+            <span>
+              (<p>m</p>
+              <sub>nz</sub>)
+            </span>
+          </Output>
+          <Output val={state.outputVal[7]}>
+            Wartość funkcji celu <span>(F(m))</span>
+          </Output>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Main;
--- frontend/src/Components/Output/Output.js
+++ frontend/src/Components/Output/Output.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import './Output.scss'
+import './Output.scss';
 
-const Output = (props) => {
-    return (
-        <div className="output">
-            <p className="output__label" for={props.name}>{props.children}</p>
-            <div className="output__output" type="text">&nbsp;</div>
-        </div>
-    )
-}
+const Output = props => {
+  return (
+    <div className="output">
+      <p className="output__label" for={props.name}>
+        {props.children}
+      </p>
+      <div className="output__output" type="text">
+        {props.val ? props.val.toFixed(2) : '\xa0'}
+      </div>
+    </div>
+  );
+};
 
 export default Output;
